7354fc7ea26f45d96602b9e900ea5d78d67637ca
Marius Bagle <marius@NTNU24923>
2026-06-08 10:08:37 +0200

    latest

--- models/Resources/append_int_gains_forecast.py
+++ models/Resources/append_int_gains_forecast.py
@@ -3,9 +3,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 forecast = pd.read_csv("internal_setpoints_occupancy.csv", index_col=0)
-forecast.drop(columns=["reaAuxPow"], inplace=True)
-forecast["reaAuxPow[1]"] = 0
-forecast["reaAuxPow[2]"] = 0
+forecast.drop(columns=["reaAuxPow[1]", "reaAuxPow[2]"], inplace=True)
+#forecast["reaAuxPow[1]"] = 0
+#forecast["reaAuxPow[2]"] = 0
 forecast.to_csv("internal_setpoints_occupancy.csv", index=True)
 
 print(forecast)
