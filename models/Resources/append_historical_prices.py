46e39efb8ceab7b9a32c83589ffd83333c8f871a
Marius Bagle <marius@NTNU24923>
2026-07-01 11:33:43 +0200

    recompiled FMU with prices from 2026 (until 30.06) and from 2022 (01.07-31.12).

--- models/Resources/append_historical_prices.py
+++ models/Resources/append_historical_prices.py
@@ -3,13 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 prices = pd.read_csv("prices_old.csv", index_col=0)
-hist = pd.read_csv("prices_2022-01-01_to_2026-06-08_NO3.csv", index_col=0)
-hist.index = pd.to_datetime(hist.index).tz_localize(None)
-#hist = hist.resample(rule="15min").ffill()
+first = pd.read_csv("prices_2026-01-01_to_2026-06-30_NO3.csv", index_col=0)
+second = pd.read_csv("prices_2022-01-01_to_2022-12-31_NO3.csv", index_col=0)
+first.index = pd.to_datetime(first.index).tz_localize(None)
+second.index = pd.to_datetime(second.index).tz_localize(None)
+# convert to timedelta:
+first.index = first.index - first.index[0]
+second.index = second.index - second.index[0]
+# upsample second:
+second = second.resample(rule="15min").ffill()
+split = first.index[-1] + pd.Timedelta(minutes=15)
 
-# get 2026:
-this_year_inds = [ndx for ndx in hist.index if ndx.year == 2026]
-this_year = hist.loc[this_year_inds]
+hist = pd.concat([
+    first, second.loc[split:]
+])
 """
 last_year_inds = [ndx for ndx in hist.index if ndx.year == 2025]
 last_year = hist.loc[last_year_inds]
@@ -22,11 +29,9 @@ comb = pd.concat([this_year, last_year.loc[merge_ndx:]])
 comb.index = range(0,len(comb.index)*900, 900)
 """
 
-this_year.index = np.arange(0, len(this_year)*900.0, 900.0)
-prices["PriceElectricHistorical"] = 1
-prices.loc[this_year.index, "PriceElectricHistorical"] = this_year.values/1000
-prices["PriceElectricPowerHighlyDynamic"] = prices["PriceElectricHistorical"] 
-prices.drop(columns=["PriceElectricHistorical"], inplace=True)
+hist.index = np.arange(0, len(hist)*900.0, 900.0)
+prices["PriceElectricPowerHighlyDynamic"] = hist/1000
+prices["PriceElectricPowerHighlyDynamic"] = prices["PriceElectricPowerHighlyDynamic"].ffill()
 prices.to_csv("prices.csv", index=True)
 
 print(hist)
